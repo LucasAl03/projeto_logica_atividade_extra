a2d0240a85ea38f41db28bd9b0b33d5b44ab9c71
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-06-12 21:38:41 -0300

    Alterações minimas nos arquivos da questao 02

--- js/script_extra02.js
+++ js/script_extra02.js
@@ -7,13 +7,6 @@ formDados.addEventListener('submit', (evt)=>{
     evt.preventDefault()
     const form_num = new FormData(formDados)
 
-    let code = `ABCD1234`
-    let password = `1234`
-
-    if((codigo = code)&&(senha = password)){
-        divResultado.innerHTML = `ACESSO PERMITIDO`
-    }else{
-        divResultado.innerHTML = `ACESSO NEGADO`
-    }
+    
 
 })
